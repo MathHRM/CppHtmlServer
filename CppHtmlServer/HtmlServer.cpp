60111ddab011a6ebc974f58b993b439e4363d1fb
Matheus <matheushrm614@gmail.com>
2024-03-28 07:26:18 -0300

    Add project files.

---
+++ CppHtmlServer/HtmlServer.cpp
@@ -0,0 +1,17 @@
+// Imports
+#include <iostream>
+#include <tchar.h>
+
+// API imports
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+// Buffer 
+#define Buffer 1500
+// A Pragma comment. Loading Library 
+#pragma comment(lib, "ws2_32.lib")
+
+
+int main() {
+	printf("opa");
+}
