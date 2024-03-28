3771cf1645ef0358e98f5b6c50105863d5506947
Matheus <matheushrm614@gmail.com>
2024-03-28 08:31:06 -0300

    added message if listening error

--- CppHtmlServer/HtmlServer.cpp
+++ CppHtmlServer/HtmlServer.cpp
@@ -14,7 +14,7 @@
 int main() {
 	// variables
 	const int BUFFER = 1500;
-	const int PORT = 80;
+	const int PORT = 8080;
 
 	struct sockaddr_in server;
 	SOCKET server_socket;
@@ -23,7 +23,7 @@ int main() {
 	WSADATA wsa; // WSA( Windows Socket Api )
 	int result;
 
-	const char message[BUFFER] = "Message to be displayed\r";
+	const char message[BUFFER] = "mensafe";
 	char reply[BUFFER] = { 0 };
 
 	
@@ -59,10 +59,9 @@ int main() {
 
 	server.sin_family = AF_INET;
 	server.sin_port = htons( PORT ); // Defining PORT
-	InetPton(AF_INET, TEXT( "0.0.0.0" ), &server.sin_addr); // Defining The Network Address to Run the Server on
+	InetPton(AF_INET, _T( "0.0.0.0" ), &server.sin_addr); // Defining The Network Address to Run the Server on
 
 	result = bind(server_socket, (struct sockaddr*)&server, sizeof(server)); // binding the Host Address and Port Number
-	
 	if (result == SOCKET_ERROR)
 	{
 		std::cerr << "Bind Error " << WSAGetLastError() << std::endl;
@@ -70,8 +69,15 @@ int main() {
 		return 1;
 	}
 
+
+	result = listen(server_socket, AF_INET); //  Start Listening for incoming Connections.
+	if (result != 0)
+	{
+		std::cerr << "Listen failed\n";
+		getchar();
+		return 1;
+	}
 	std::cout << "Listening on : https://localhost:" << PORT << std::endl; // Tell the User we Started Listening.
-	result = listen(server_socket, AF_INET); // Then Actually Start Listening for incoming Connections.
 
 
 	client_socket = accept(server_socket, NULL, NULL);
@@ -84,11 +90,13 @@ int main() {
 
 	std::cout << "A Client Connected. Sending a Message and closing Connection" << std::endl; // notify user that someone connected
 	send(client_socket, message, strlen(message), NULL); // Send Client a Message
-
 	recv(client_socket, reply, sizeof(reply), NULL); // Just in case if the Client sends something, We Receive it.
+
 
 	closesocket(client_socket); // close the Client Socket now that our Work is Complete.
 	WSACleanup(); // Clean Windows Socket API.
+
+
 
 	getchar();
 	return 0;
